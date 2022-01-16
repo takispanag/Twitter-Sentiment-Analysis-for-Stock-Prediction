b54afe8bcb57f7de71c7542b6e85041e4e245cf1
Takis <takis.panagiot1@gmail.com>
2022-01-16 20:08:30 +0200

    Added XGBOOST Regressor

--- linear_pipeline.py
+++ linear_pipeline.py
@@ -3,31 +3,31 @@ from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_percentage_error, mean_absolute_error
 from sklearn.model_selection import train_test_split
-
+from xgboost import XGBRegressor
 import config
 from data_preprocessing import inference_vector_creation, training_vector_creation
 from data_serializer import load_pickle, save_pickle
 
 
-def linear_training(df, company_name):
-	param = "linear"
-	model = LinearRegression()
+def model_training(df, company_name, model_name, model):
 	x_train, x_test, y_train, y_test = train_test_split(df[["Sentiment", "Close"]], df["Next_Close"],
 														train_size=0.75, random_state=11)
 	model.fit(x_train, y_train)
-	save_pickle(company_name, param, model, "models")
-	linear_results(company_name, x_train, y_train, "train")
-	linear_results(company_name, x_test, y_test, "test")
+	save_pickle(company_name, model_name, model, "models")
+	model_results(company_name, x_train, y_train, "train", model_name)
+	model_results(company_name, x_test, y_test, "test", model_name)
 
-def linear_results(company, x, y, mode):
-	prediction = load_pickle(company, "linear", "models").predict(x)
+
+def model_results(company, x, y, mode, model_name):
+	prediction = load_pickle(company, model_name, "models").predict(x)
 	prediction = pd.DataFrame(prediction, index=y.index, columns=["Prediction"]).sort_index()
 	y = y.sort_index().rename("Actual Close")
 	fig, ax = plt.subplots()
 	prediction.plot(kind="line", ax=ax)
 	y.plot(kind="line", ax=ax)
-	ax.set(xlabel='Time', ylabel='Value', title=f'Linear Regression Pred vs Actual {company}')
+	ax.set(xlabel='Time', ylabel='Value', title=f'{model_name} Regression Pred vs Actual {company}')
 	plt.legend(loc="upper right")
-	plt.savefig(f"charts/{mode}/linear_pred_{company}.png")
+	plt.savefig(f"charts/{mode}/{model_name}_pred_{company}.png")
 	# plt.show()
-	print(f"{company}: MAE = {mean_absolute_error(y, prediction)}, MAP = {mean_absolute_percentage_error(y, prediction)}")
+	print(
+		f"{company}: MAE = {mean_absolute_error(y, prediction)}, MAP = {mean_absolute_percentage_error(y, prediction)}")
--- main.py
+++ main.py
@@ -6,24 +6,29 @@ import pandas as pd
 import config
 from data_preprocessing import training_vector_creation
 from data_serializer import save_pickle, load_pickle
-from linear_pipeline import linear_results, linear_training
+from linear_pipeline import model_results, model_training
 from lstm import lstm_trainer
 from lstm.lstm_pipeline import lstm_results
 from sentiment_analysis import get_historical_sentiment
 from stock_prices_acquisition import stock_prices
+from xgboost import XGBRegressor
 
 
 def train_models():
+	#Linear, XGBOOST train/test
 	for company_name in config.company_names:
 		training_vector_creation(company_name)
 		df = pd.read_csv(f"data/training_data/{company_name}_data.csv", index_col=0)
-		# linear_training(df, company_name)
-		#train lstm
-		train_data = df.iloc[:int(df.shape[0] * 0.9)] # get 90% of data for training/validation
-		# lstm_trainer.train(train_data, company_name)
-		lstm_results(company_name, train_data, "train")
-		test_data = df.iloc[int(df.shape[0] * 0.9):] # get 10% of data for prediction:)
-		lstm_results(company_name, test_data, "test")
+		model_names = ["Linear", "XGBoost"]
+		models = [LinearRegression(), XGBRegressor()]
+		for model_name, model in zip(model_names, models):
+			model_training(df, company_name, model_name, model)
+	# train lstm
+	# train_data = df.iloc[:int(df.shape[0] * 0.9)] # get 90% of data for training/validation
+	# # lstm_trainer.train(train_data, company_name)
+	# lstm_results(company_name, train_data, "train")
+	# test_data = df.iloc[int(df.shape[0] * 0.9):] # get 10% of data for prediction:)
+	# lstm_results(company_name, test_data, "test")
 
 
 if __name__ == "__main__":
